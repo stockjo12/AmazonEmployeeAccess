b167b36d319aec649c2ae58fe8774aa1fc3a716e
stockjo12 <stockjo12@gmail.com>
2025-10-29 11:31:14 -0600

    Principle Component Reduction
    Running Principle Component Reduction

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -37,90 +37,90 @@ amazon_recipe <- recipe(action ~ ., data = train) |>
 prep <- prep(amazon_recipe)
 baked <- bake(prep, new_data = test)
 
-# (1) Making Principal Component Reduction Recipe
-target <- "action"
-ev <- c("role_title", "role_rollup_2", "role_rollup_1", "role_family_desc",
-        "role_family", "role_deptname", "role_code", "resource", "mgr_id")
-pcr_recipe <- recipe(action ~ ., data = train) |>
-  step_mutate_at(any_of(ev), fn = factor) |>
-  step_other(any_of(ev), threshold = 0.1) |> #0.1 for Testing; 0.001 for Results
-  step_dummy(any_of(ev)) |>
-  step_normalize(all_numeric_predictors()) |>
-  step_pca(all_predictors(), threshold = 0.9)
-prep <- prep(pcr_recipe)
-baked <- bake(prep, new_data = test)
-
-### WORK FLOWS ###
-# (1) Logistic Regression
-#Defining Model
-logRegModel <- logistic_reg() |>
-  set_engine("glm")
-
-#Making Workflow
-log_workflow <- workflow() |>
-  add_recipe(pcr_recipe) |>
-  add_model(logRegModel) |>
-  fit(data = train)
-
-#Making Predictions
-amazon_predictions <- predict(log_workflow,
-                              new_data=test,
-                              type = "prob")
-
-#Formatting Predictions for Kaggle
-kaggle_log <- amazon_predictions |>
-  bind_cols(test) |>
-  select(id, .pred_1) |>
-  rename(action = .pred_1)
-
-#Saving CSV File
-vroom_write(x=kaggle_log, file="./Logistic(2).csv", delim=",")
-
-# (2) Penalized Logistic Regression
-#Defining Model
-amazon_plog <- logistic_reg(mixture = tune(), penalty = tune()) |>
-  set_engine("glmnet")
-
-#Making Workflow
-plog_workflow <- workflow() |>
-  add_recipe(pcr_recipe) |>
-  add_model(amazon_plog)
-
-#Defining Grid of Values
-tuning_grid <- grid_regular(penalty(),
-                            mixture(),
-                            levels = 3) #3 for Testing; 5 for Results
-
-#Splitting Data
-folds <- vfold_cv(train,
-                  v = 5,
-                  repeats = 1) #1 for Testing; 3 for Results
-
-#Run Cross Validation
-CV_results <- plog_workflow |>
-  tune_grid(resamples = folds, grid = tuning_grid,
-            metrics = metric_set(roc_auc))
-
-#Find Best Tuning Parameters
-bestTune <- CV_results |>
-  select_best(metric = "roc_auc")
-
-#Finalize Workflow
-final_wf <- plog_workflow |>
-  finalize_workflow(bestTune) |>
-  fit(data = train)
-
-#Making Predictions
-plog_pred <- predict(final_wf, new_data = test, type = "prob")
-
-#Formatting Predictions for Kaggle
-kaggle_plog <- plog_pred |>
-  bind_cols(test) |>
-  select(id, .pred_1) |>
-  rename(action = .pred_1)
-
-#Saving CSV File
-vroom_write(x=kaggle_plog, file="./Penalized_PCR.csv", delim=",")
+# # (1) Making Principal Component Reduction Recipe
+# target <- "action"
+# ev <- c("role_title", "role_rollup_2", "role_rollup_1", "role_family_desc",
+#         "role_family", "role_deptname", "role_code", "resource", "mgr_id")
+# pcr_recipe <- recipe(action ~ ., data = train) |>
+#   step_mutate_at(any_of(ev), fn = factor) |>
+#   step_other(any_of(ev), threshold = 0.1) |> #0.1 for Testing; 0.001 for Results
+#   step_dummy(any_of(ev)) |>
+#   step_normalize(all_numeric_predictors()) |>
+#   step_pca(all_predictors(), threshold = 0.9)
+# prep <- prep(pcr_recipe)
+# baked <- bake(prep, new_data = test)
+# 
+# ### WORK FLOWS ###
+# # (1) Logistic Regression
+# #Defining Model
+# logRegModel <- logistic_reg() |>
+#   set_engine("glm")
+# 
+# #Making Workflow
+# log_workflow <- workflow() |>
+#   add_recipe(pcr_recipe) |>
+#   add_model(logRegModel) |>
+#   fit(data = train)
+# 
+# #Making Predictions
+# amazon_predictions <- predict(log_workflow,
+#                               new_data=test,
+#                               type = "prob")
+# 
+# #Formatting Predictions for Kaggle
+# kaggle_log <- amazon_predictions |>
+#   bind_cols(test) |>
+#   select(id, .pred_1) |>
+#   rename(action = .pred_1)
+# 
+# #Saving CSV File
+# vroom_write(x=kaggle_log, file="./Logistic(2).csv", delim=",")
+# 
+# # (2) Penalized Logistic Regression
+# #Defining Model
+# amazon_plog <- logistic_reg(mixture = tune(), penalty = tune()) |>
+#   set_engine("glmnet")
+# 
+# #Making Workflow
+# plog_workflow <- workflow() |>
+#   add_recipe(pcr_recipe) |>
+#   add_model(amazon_plog)
+# 
+# #Defining Grid of Values
+# tuning_grid <- grid_regular(penalty(),
+#                             mixture(),
+#                             levels = 3) #3 for Testing; 5 for Results
+# 
+# #Splitting Data
+# folds <- vfold_cv(train,
+#                   v = 5,
+#                   repeats = 1) #1 for Testing; 3 for Results
+# 
+# #Run Cross Validation
+# CV_results <- plog_workflow |>
+#   tune_grid(resamples = folds, grid = tuning_grid,
+#             metrics = metric_set(roc_auc))
+# 
+# #Find Best Tuning Parameters
+# bestTune <- CV_results |>
+#   select_best(metric = "roc_auc")
+# 
+# #Finalize Workflow
+# final_wf <- plog_workflow |>
+#   finalize_workflow(bestTune) |>
+#   fit(data = train)
+# 
+# #Making Predictions
+# plog_pred <- predict(final_wf, new_data = test, type = "prob")
+# 
+# #Formatting Predictions for Kaggle
+# kaggle_plog <- plog_pred |>
+#   bind_cols(test) |>
+#   select(id, .pred_1) |>
+#   rename(action = .pred_1)
+# 
+# #Saving CSV File
+# vroom_write(x=kaggle_plog, file="./Penalized_PCR.csv", delim=",")
 
 # (3) Random Forests
 #Defining Model
@@ -173,168 +173,168 @@ kaggle_forest <- forest_pred |>
 #Saving CSV File
 vroom_write(x=kaggle_forest, file="./Forest_BATCH.csv", delim=",")
 
-# (4) K-Nearest Neighbors
-#Defining Model
-knn_model <- nearest_neighbor(neighbors = tune()) |>
-  set_engine("kknn") |>
-  set_mode("classification")
-
-#Creating a Workflow
-knn_wf <- workflow() |>
-  add_recipe(pcr_recipe)|>
-  add_model(knn_model)
-
-#Defining Grid of Values
-knn_grid <- grid_regular(neighbors(range = c(1, 250)),
-                            levels = 5) #More Levels = More Time
-
-#Splitting Data
-knn_folds <- vfold_cv(train,
-                         v = 5,
-                         repeats = 1) #1 for Testing; 3 for Results
-
-#Run Cross Validation
-knn_results <- knn_wf |>
-  tune_grid(resamples = knn_folds,
-            grid = knn_grid,
-            metrics = metric_set(roc_auc))
-
-#Find Best Tuning Parameters
-knn_best <- knn_results |>
-  select_best(metric = "roc_auc")
-
-#Finalizing Workflow
-final_kwf <- knn_wf |>
-  finalize_workflow(knn_best) |>
-  fit(data = train)
-
-#Making Predictions
-knn_pred <- predict(final_kwf, new_data = test, type = "prob")
-
-#Formatting Predictions for Kaggle
-kaggle_knn <- knn_pred |>
-  bind_cols(test) |>
-  select(id, .pred_1) |>
-  rename(action = .pred_1)
-
-#Saving CSV File
-vroom_write(x=kaggle_knn, file="./KNN_PCR.csv", delim=",")
-
-# (5) Naive Bayes
-#Defining Model
-bayes_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) |>
-  set_engine("naivebayes") |>
-  set_mode("classification")
-
-#Creating a Workflow
-bayes_wf <- workflow() |>
-  add_recipe(amazon_recipe)|>
-  add_model(bayes_model)
-
-#Defining Grid of Values
-bayes_grid <- grid_regular(Laplace(range = c(0, 2)),
-                           smoothness(range = c(0.01, 1)),
-                           levels = 5) #3 for Testing; 5 for Results
-
-#Splitting Data
-bayes_folds <- vfold_cv(train,
-                      v = 5,
-                      repeats = 3) #1 for Testing; 3 for Results
-
-#Run Cross Validation
-bayes_results <- bayes_wf |>
-  tune_grid(resamples = bayes_folds,
-            grid = bayes_grid,
-            metrics = metric_set(roc_auc))
-
-#Find Best Tuning Parameters
-bayes_best <- bayes_results |>
-  select_best(metric = "roc_auc")
-
-#Finalizing Workflow
-final_bwf <- bayes_wf |>
-  finalize_workflow(bayes_best) |>
-  fit(data = train)
-
-#Making Predictions
-bayes_pred <- predict(final_bwf, new_data = test, type = "prob")
-
-#Formatting Predictions for Kaggle
-kaggle_bayes <- bayes_pred |>
-  bind_cols(test) |>
-  select(id, .pred_1) |>
-  rename(action = .pred_1)
-
-#Saving CSV File
-vroom_write(x = kaggle_bayes, file="./Bayes.csv", delim=",")
-
-# (6) Neural Networks
-#Making Recipe
-target <- "action" 
-ev <- c("role_title", "role_rollup_2", "role_rollup_1", "role_family_desc", 
-        "role_family", "role_deptname", "role_code", "resource", "mgr_id")
-nn_recipe <- recipe(formula = action ~ ., data = train) |>
-  step_mutate_at(any_of(ev), fn = factor) |>
-  step_other(any_of(ev), threshold = 0.1) |> #0.1 for Testing; 0.001 for Results
-  step_lencode_glm(any_of(ev), outcome = target) |>
-  step_normalize(all_numeric_predictors()) 
-
-#Defining Model
-nn_model <- mlp(hidden_units = tune(),
-                epochs = 50) |> #50 Low, 100 Medium, 250 High
-  set_engine("keras") |>
-  set_mode("classification")
-
-#Creating a Workflow
-nn_wf <- workflow() |>
-  add_recipe(nn_recipe)|>
-  add_model(nn_model)
-
-#Defining Grid of Values
-nn_grid <- grid_regular(hidden_units(range = c(1, 20)),
-                           levels = 3) #3 for Testing; 5 for Results
-
-#Splitting Data
-nn_folds <- vfold_cv(train,
-                      v = 5,
-                      repeats = 1) #1 for Testing; 3 for Results
-
-#Run Cross Validation
-nn_results <- nn_wf |>
-  tune_grid(resamples = nn_folds,
-            grid = nn_grid,
-            metrics = metric_set(roc_auc))
-
-#Find Best Tuning Parameters
-nn_best <- nn_results |>
-  select_best(metric = "roc_auc")
-
-#Finalizing Workflow
-final_nwf <- nn_wf |>
-  finalize_workflow(nn_best) |>
-  fit(data = train)
-
-#Making Predictions
-nn_pred <- predict(final_nwf, new_data = test, type = "prob")
-
-#Formatting Predictions for Kaggle
-kaggle_nn <- nn_pred |>
-  bind_cols(test) |>
-  select(id, .pred_1) |>
-  rename(action = .pred_1)
-
-#Saving CSV File
-vroom_write(x = kaggle_nn, file="./NN_BATCH.csv", delim=",")
-
-#Making Plot
-nn_results |> collect_metrics() |>
-  filter(.metric == "roc_auc") |>
-  ggplot(aes(x = hidden_units, y = mean)) +
-  geom_line() + 
-  geom_point() +
-  labs(title = "ROC AUC Vs Hidden Units",
-       x = "Hidden Units",
-       y = "Mean ROC AUC")
+# # (4) K-Nearest Neighbors
+# #Defining Model
+# knn_model <- nearest_neighbor(neighbors = tune()) |>
+#   set_engine("kknn") |>
+#   set_mode("classification")
+# 
+# #Creating a Workflow
+# knn_wf <- workflow() |>
+#   add_recipe(pcr_recipe)|>
+#   add_model(knn_model)
+# 
+# #Defining Grid of Values
+# knn_grid <- grid_regular(neighbors(range = c(1, 250)),
+#                             levels = 5) #More Levels = More Time
+# 
+# #Splitting Data
+# knn_folds <- vfold_cv(train,
+#                          v = 5,
+#                          repeats = 1) #1 for Testing; 3 for Results
+# 
+# #Run Cross Validation
+# knn_results <- knn_wf |>
+#   tune_grid(resamples = knn_folds,
+#             grid = knn_grid,
+#             metrics = metric_set(roc_auc))
+# 
+# #Find Best Tuning Parameters
+# knn_best <- knn_results |>
+#   select_best(metric = "roc_auc")
+# 
+# #Finalizing Workflow
+# final_kwf <- knn_wf |>
+#   finalize_workflow(knn_best) |>
+#   fit(data = train)
+# 
+# #Making Predictions
+# knn_pred <- predict(final_kwf, new_data = test, type = "prob")
+# 
+# #Formatting Predictions for Kaggle
+# kaggle_knn <- knn_pred |>
+#   bind_cols(test) |>
+#   select(id, .pred_1) |>
+#   rename(action = .pred_1)
+# 
+# #Saving CSV File
+# vroom_write(x=kaggle_knn, file="./KNN_PCR.csv", delim=",")
+# 
+# # (5) Naive Bayes
+# #Defining Model
+# bayes_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) |>
+#   set_engine("naivebayes") |>
+#   set_mode("classification")
+# 
+# #Creating a Workflow
+# bayes_wf <- workflow() |>
+#   add_recipe(amazon_recipe)|>
+#   add_model(bayes_model)
+# 
+# #Defining Grid of Values
+# bayes_grid <- grid_regular(Laplace(range = c(0, 2)),
+#                            smoothness(range = c(0.01, 1)),
+#                            levels = 5) #3 for Testing; 5 for Results
+# 
+# #Splitting Data
+# bayes_folds <- vfold_cv(train,
+#                       v = 5,
+#                       repeats = 3) #1 for Testing; 3 for Results
+# 
+# #Run Cross Validation
+# bayes_results <- bayes_wf |>
+#   tune_grid(resamples = bayes_folds,
+#             grid = bayes_grid,
+#             metrics = metric_set(roc_auc))
+# 
+# #Find Best Tuning Parameters
+# bayes_best <- bayes_results |>
+#   select_best(metric = "roc_auc")
+# 
+# #Finalizing Workflow
+# final_bwf <- bayes_wf |>
+#   finalize_workflow(bayes_best) |>
+#   fit(data = train)
+# 
+# #Making Predictions
+# bayes_pred <- predict(final_bwf, new_data = test, type = "prob")
+# 
+# #Formatting Predictions for Kaggle
+# kaggle_bayes <- bayes_pred |>
+#   bind_cols(test) |>
+#   select(id, .pred_1) |>
+#   rename(action = .pred_1)
+# 
+# #Saving CSV File
+# vroom_write(x = kaggle_bayes, file="./Bayes.csv", delim=",")
+# 
+# # (6) Neural Networks
+# #Making Recipe
+# target <- "action" 
+# ev <- c("role_title", "role_rollup_2", "role_rollup_1", "role_family_desc", 
+#         "role_family", "role_deptname", "role_code", "resource", "mgr_id")
+# nn_recipe <- recipe(formula = action ~ ., data = train) |>
+#   step_mutate_at(any_of(ev), fn = factor) |>
+#   step_other(any_of(ev), threshold = 0.1) |> #0.1 for Testing; 0.001 for Results
+#   step_lencode_glm(any_of(ev), outcome = target) |>
+#   step_normalize(all_numeric_predictors()) 
+# 
+# #Defining Model
+# nn_model <- mlp(hidden_units = tune(),
+#                 epochs = 50) |> #50 Low, 100 Medium, 250 High
+#   set_engine("keras") |>
+#   set_mode("classification")
+# 
+# #Creating a Workflow
+# nn_wf <- workflow() |>
+#   add_recipe(nn_recipe)|>
+#   add_model(nn_model)
+# 
+# #Defining Grid of Values
+# nn_grid <- grid_regular(hidden_units(range = c(1, 20)),
+#                            levels = 3) #3 for Testing; 5 for Results
+# 
+# #Splitting Data
+# nn_folds <- vfold_cv(train,
+#                       v = 5,
+#                       repeats = 1) #1 for Testing; 3 for Results
+# 
+# #Run Cross Validation
+# nn_results <- nn_wf |>
+#   tune_grid(resamples = nn_folds,
+#             grid = nn_grid,
+#             metrics = metric_set(roc_auc))
+# 
+# #Find Best Tuning Parameters
+# nn_best <- nn_results |>
+#   select_best(metric = "roc_auc")
+# 
+# #Finalizing Workflow
+# final_nwf <- nn_wf |>
+#   finalize_workflow(nn_best) |>
+#   fit(data = train)
+# 
+# #Making Predictions
+# nn_pred <- predict(final_nwf, new_data = test, type = "prob")
+# 
+# #Formatting Predictions for Kaggle
+# kaggle_nn <- nn_pred |>
+#   bind_cols(test) |>
+#   select(id, .pred_1) |>
+#   rename(action = .pred_1)
+# 
+# #Saving CSV File
+# vroom_write(x = kaggle_nn, file="./NN_BATCH.csv", delim=",")
+# 
+# #Making Plot
+# nn_results |> collect_metrics() |>
+#   filter(.metric == "roc_auc") |>
+#   ggplot(aes(x = hidden_units, y = mean)) +
+#   geom_line() + 
+#   geom_point() +
+#   labs(title = "ROC AUC Vs Hidden Units",
+#        x = "Hidden Units",
+#        y = "Mean ROC AUC")
 
 ### EDA ### 
 #Wrangling Data for EDA
